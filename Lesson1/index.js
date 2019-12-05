f940e9026a6353dcd972b1c550683de5be223e37
Kirill Borisov <kirill-borisov@yandex.ru>
2019-12-05 10:21:05 +0300

    Homework Lesson1

---
+++ Lesson1/index.js
@@ -0,0 +1,37 @@
+const goods = [
+  { title: "Shirt", price: 100 },
+  { title: "Socks", price: 120 },
+  { title: "Jacket", price: 150 },
+  { title: "Shoes", price: 180 },
+  { title: "Blues", price: 280 },
+  { title: "T-shirt", price: 39.99 },
+  { title: "Cardigan", price: 200 },
+  { title: "Kimono", price: 111 },
+  { title: "Cardigan", price: 200 },
+  { title: "Suit", price: 340 }
+];
+
+const renderGoodsItem = (title, price) => {
+  return `<div class="goods-item"><div class="goods-item-in">
+      <img class="goods-item-img" src="NoImage.png"/>
+      <div class="goods-item-name">${title}</div>
+      <div class="goods-item-price">${price}</div>
+      <div class="goods-item-desc">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </div>
+      <div class="goods-item-addbtn-container"><button>Добавить</button></div>
+      </div></div>`;
+};
+
+const renderGoodsList = list => {
+  let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+  /*
+    document.querySelector(".goods-list").innerHTML = goodsList;
+    При таком варианте в браузере отображаются запятые после каждого "goods-item".
+    Это происходит из-за того, что goodsList это массив, который по умолчанию преобразуется в строку с разделением элементов запятой.
+    Исправить это можно, преобразовав массив с строку без разделения элементов запятыми с помощью функции join
+  */
+  document.querySelector(".goods-list").innerHTML = goodsList.join("");
+};
+
+renderGoodsList(goods);
